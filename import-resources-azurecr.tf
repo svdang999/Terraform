ce1faeb5655dda2a4c409cc47b2bbf723fcdb6a5
thosewerethedays999 <101574223+thosewerethedays99@users.noreply.github.com>
2022-12-22 15:56:54 +0700

    Create import-resources-azurecr.tf

---
+++ import-resources-azurecr.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.37.0" #https://registry.terraform.io/providers/hashicorp/azurerm/latest
+    }
+  }
+    backend "azurerm" {
+        resource_group_name  = "rg-infra-dev"
+        storage_account_name = "tfstatesc9qu"
+        container_name       = "tfstate"
+        key                  = "terraform_azurecr.tfstate"
+    }
+
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id   = "xxx" 
+  tenant_id         = "xxx" 
+  client_id         = "xxx" 
+  client_secret     = "xxx"
+}
+
+resource "azurerm_container_registry" "azurecr_import" {
+  # (resource arguments)
+}
